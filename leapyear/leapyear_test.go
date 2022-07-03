8235d1d53d23252dc9e031462d66067d842020f8
Samuel Taboas <droidsam@gmail.com>
2022-07-03 12:30:07 +0200

    refactor test for improved semantics

--- leapyear/leapyear_test.go
+++ leapyear/leapyear_test.go
@@ -2,18 +2,20 @@ package leapyear
 
 import "testing"
 
-func TestLeapYearShouldBeAFunction(t *testing.T) {
-	got := LeapYear(1995)
+func TestLeapYear(t *testing.T) {
+	t.Run("should return a boolean", func(t *testing.T) {
+		got := LeapYear(1995)
 
-	if got != false {
-		t.Fatalf("Expected LeapYear to return a boolean")
-	}
-}
+		if got != false {
+			t.Fatalf("Expected LeapYear to return a boolean")
+		}
+	})
 
-func TestLeapYearShouldReturnFalseForNonLeapYears(t *testing.T) {
-	got := LeapYear(1995)
+	t.Run("should return false when parameter is a common year", func(t *testing.T) {
+		got := LeapYear(1995)
 
-	if got != false {
-		t.Fatalf("Expected LeapYear to return false")
-	}
+		if got != false {
+			t.Fatalf("Expected LeapYear to return false")
+		}
+	})
 }
