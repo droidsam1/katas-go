ce91d068be6a3ad8745e02d71401750af7dc19cf
Samuel Taboas <droidsam@gmail.com>
2022-07-05 21:35:00 +0200

    add fizz test iteration

--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -13,12 +13,27 @@ func TestFizzBuzz(t *testing.T) {
 		got := FizzBuzz(input)
 
 		if got != "1" {
-			t.Fatalf("expect to return the number itself")
+			t.Fatalf("expected to return the number itself")
 		}
+	})
+
+	t.Run("should return Fizz for multiples of three", func(t *testing.T) {
+
+		input := 3
+
+		got := FizzBuzz(input)
+
+		if got != "Fizz" {
+			t.Fatalf("expected to return the word Fizz")
+		}
+
 	})
 
 }
 
 func FizzBuzz(input int) string {
+	if input%3 == 0 {
+		return "Fizz"
+	}
 	return strconv.Itoa(input)
 }
