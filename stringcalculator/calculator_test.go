65c18093872e0f0c46562fb7ea0e2a63c073a4c0
Samuel Taboas <droidsam@gmail.com>
2022-07-06 19:06:51 +0200

    refactor tests to improve readability

--- stringcalculator/calculator_test.go
+++ stringcalculator/calculator_test.go
@@ -7,21 +7,30 @@ import (
 func TestCalculator(t *testing.T) {
 
 	t.Run("should be implemented by a method Add", func(t *testing.T) {
+
 		got := Add("")
 
-		assertEquals(0, got, t)
+		expected := 0
+
+		assertEquals(expected, got, t)
 
 	})
 	t.Run("should accept a string wih a single number as input parameter", func(t *testing.T) {
+
 		got := Add("1")
 
-		assertEquals(1, got, t)
+		expected := 1
+
+		assertEquals(expected, got, t)
 	})
 
 	t.Run("should return the sum when a string wih a two numbers ", func(t *testing.T) {
+
 		got := Add("1,2")
 
-		assertEquals(3, got, t)
+		expected := 3
+
+		assertEquals(expected, got, t)
 	})
 }
 
