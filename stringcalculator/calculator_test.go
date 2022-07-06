9e5db24d0aad3bf2b93d0c42a75aab91343163e7
Samuel Taboas <droidsam@gmail.com>
2022-07-06 19:04:44 +0200

    refactor test to use the assert func

--- stringcalculator/calculator_test.go
+++ stringcalculator/calculator_test.go
@@ -9,25 +9,19 @@ func TestCalculator(t *testing.T) {
 	t.Run("should be implemented by a method Add", func(t *testing.T) {
 		got := Add("")
 
-		if got != 0 {
-			t.Fatalf("expected to return a integer")
-		}
+		assertEquals(0, got, t)
 
 	})
 	t.Run("should accept a string wih a single number as input parameter", func(t *testing.T) {
 		got := Add("1")
 
-		if got != 1 {
-			t.Fatalf("expected to return the same number")
-		}
+		assertEquals(1, got, t)
 	})
 
 	t.Run("should return the sum when a string wih a two numbers ", func(t *testing.T) {
 		got := Add("1,2")
 
-		if got != 3 {
-			t.Fatalf("expected to return 3")
-		}
+		assertEquals(3, got, t)
 	})
 }
 
